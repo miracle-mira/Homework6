15cd0451aec27ef0b43f490d991bbf9daf250f43
unknown <pmrim01@chungbuk.ac.kr>
2023-04-26 02:22:17 +0900

    HW6 주석추가

--- SingleLinkedList.c
+++ SingleLinkedList.c
@@ -1,11 +1,6 @@
 /*
- * singly linked list
- *
- *  Data Structures
- *  School of Computer Science 
- *  at Chungbuk National University
+ * 단일 연결 리스트
  */
-
 
 #include<stdio.h>
 #include<stdlib.h>
